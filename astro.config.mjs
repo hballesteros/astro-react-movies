daf6e63ce41cd313cc0beab7150581716230f52c
Hugo <hugo@boahstudio.com>
2023-05-25 00:29:49 -0300

    search with React js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import dotenv from 'dotenv';
-
+import react from "@astrojs/react";
 dotenv.config();
+
 
 // https://astro.build/config
 export default defineConfig({
-    output:"server",
-    env: {
-        TOKEN: process.env.TOKEN,
-    }
+  output: "server",
+  env: {
+    TOKEN: process.env.TOKEN
+  },
+  integrations: [react()]
 });
-
